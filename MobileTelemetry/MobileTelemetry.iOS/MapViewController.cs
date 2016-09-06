094f5e366c5ac0d0ae657f63a4bac383d1e84f5e
Gary Jackson <GaryLJackson@gmail.com>
2016-09-06 21:00:44 +1000

    Added some code to draw the path on the map

--- MobileTelemetry/MobileTelemetry.iOS/MapViewController.cs
+++ MobileTelemetry/MobileTelemetry.iOS/MapViewController.cs
@@ -1,4 +1,3 @@
-using Foundation;
 using System;
 using CoreLocation;
 using MapKit;
@@ -9,37 +8,52 @@ namespace MobileTelemetry.iOS
 {
     public partial class MapViewController : UIViewController
     {
-        private MKMapView _map;
         private readonly ILocationManager _locationManager;
+        private MKMapView _mkMapView;
+        private Models.Location _lastLocation;
+        private readonly MapDelegate _mapDelegate;
 
         public MapViewController (IntPtr handle) : base (handle)
         {
             _locationManager = LocationManager.Instance;
-
+            _mapDelegate = new MapDelegate();
         }
 
         private void LocationManagerOnLocationUpdated(object sender, Models.LocationUpdatedEventArgs e)
         {
-            _map.AddAnnotations(new MKPointAnnotation
+            if (_lastLocation == null)
             {
-                Coordinate = new CLLocationCoordinate2D(e.Location.Latitude, e.Location.Longitude)
-            });
+                _lastLocation = e.Location;
+                return;
+            }
+
+            var overlay = MKPolyline.FromCoordinates(
+                new[]
+                {
+                    new CLLocationCoordinate2D(_lastLocation.Latitude, _lastLocation.Longitude),
+                    new CLLocationCoordinate2D(e.Location.Latitude, e.Location.Longitude)
+                });
+
+            _mkMapView.AddOverlay(overlay);
+
+            _lastLocation = e.Location;
         }
 
         public override void LoadView()
         {
-            _map = new MKMapView(UIScreen.MainScreen.Bounds)
+            _mkMapView = new MKMapView(UIScreen.MainScreen.Bounds)
             {
                 ShowsUserLocation = true,
                 MapType = MKMapType.Standard,
                 ZoomEnabled = true,
-                ScrollEnabled = true
+                ScrollEnabled = true,
+                Delegate = _mapDelegate
             };
 
-            View = _map;
+            View = _mkMapView;
         }
 
-        
+
         public override void ViewDidLoad()
         {
             _locationManager.LocationUpdated += LocationManagerOnLocationUpdated;
@@ -50,6 +64,50 @@ namespace MobileTelemetry.iOS
             _locationManager.LocationUpdated -= LocationManagerOnLocationUpdated;
         }
 
+        private static double KilometresToLatitudeDegrees(double kms)
+        {
+            const double earthRadius = 6371.0; // in kms
+            const double radiansToDegrees = 180.0 / Math.PI;
+            return (kms / earthRadius) * radiansToDegrees;
+        }
+
+        private static double KilometresToLongitudeDegrees(double kms, double atLatitude)
+        {
+            const double earthRadius = 6371.0; // in kms
+            const double degreesToRadians = Math.PI / 180.0;
+            const double radiansToDegrees = 180.0 / Math.PI;
+
+            // derive the earth's radius at that point in latitude
+            var radiusAtLatitude = earthRadius * Math.Cos(atLatitude * degreesToRadians);
+            return (kms / radiusAtLatitude) * radiansToDegrees;
+        }
+
+        private class MapDelegate : MKMapViewDelegate
+        {
+            public override void DidUpdateUserLocation(MKMapView mapView, MKUserLocation userLocation)
+            {
+                if (mapView.UserLocation != null)
+                {
+                    var coords = mapView.UserLocation.Coordinate;
+                    var span = new MKCoordinateSpan(KilometresToLatitudeDegrees(2), KilometresToLongitudeDegrees(2, coords.Latitude));
+                    mapView.Region = new MKCoordinateRegion(coords, span);
+                }
+            }
+
+            public override MKOverlayView GetViewForOverlay(MKMapView mapView, IMKOverlay overlay)
+            {
+                var polygon = overlay as MKPolyline;
+
+                var polygonView = new MKPolylineView(polygon)
+                {
+                    LineWidth = 3.0f,
+                    StrokeColor = UIColor.Red
+                };
+                return polygonView;
+
+                
+            }
+        }
 
     }
 }
